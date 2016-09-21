a8b3ece4e6841befff67915174c040f79b0ad811
edgar.ahgue <fchinosk@hotmail.com>
2016-09-21 01:33:04 -0500

    finito

--- LetterNumber/ViewController.swift
+++ LetterNumber/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textBox: UITextView!
+    @IBAction func countLetters(_ sender: UIButton) {
+        
+        func wordCount (s:String)-> Dictionary<Character, Int> {
+            let myArray : String = textBox.text
+            let myChar = [Character](myArray.characters)
+            //print("my char is \(myChar)")
+            //let words = s.components(separatedBy: " ")
+            var wordDictionary = Dictionary<Character, Int>()
+            for word in myChar {
+                if wordDictionary[word] == nil {
+                    wordDictionary[word] = 1
+                }
+                else {
+                    wordDictionary.updateValue(wordDictionary[word]!+1, forKey: word)
+                }
+            }
+            print(wordDictionary)
+            return wordDictionary
+        }
+        
+        wordCount(s: textBox.text)
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
